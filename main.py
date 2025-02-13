e539a8888921cb67688298d1874a7205bc934c63
Yazkox <buatoisloic@gmail.com>
2025-02-13 13:11:30 +0900

    added nickname map

--- main.py
+++ main.py
@@ -5,37 +5,49 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from database_manager import DataManager
 from custom_poll import Poll
-import pytz
 
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = 1012323355346608198
+GUILD_ID = 1338806572125786152
 CHANNEL_NAME = "askus"
 POLL_DELTA = timedelta(hours=14)
 
+USER_MAP = {
+    165132116344963072: "François",
+    266305722076102657: "Loïc",
+    298081860230184960: "Timothée",
+    308191958319497216: "Jade",
+    334632744745435137: "Chammings",
+    380327586779234334: "Alexis",
+    389476905830711320: "Chakma",
+    407171886221361153: "Rémi",
+    411610859324833793: "Noë",
+    582631508523745282: "Sixtine",
+    639416249985794078: "Corentin",
+}
+
 def incr_time(time: datetime) -> datetime:
     return time.replace(hour=21, minute=0, second=0, microsecond=0) + timedelta(days=1)
+
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.data_base = DataManager()
-        
+
     async def setup_hook(self) -> None:
         self.my_background_task.start()
 
     async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------")
 
     async def send_poll(self, channel: discord.abc.GuildChannel, question: str, remaining: int = None):
         poll = discord.Poll(question, POLL_DELTA)
-        for member in channel.members:
-            if member.bot:
-                continue
-            username = member.nick if member.nick else member.name
-            poll = poll.add_answer(text=username)
+        choices = [self.get_nickname_map(channel).values()]
+        for choice in choices:
+            poll = poll.add_answer(text=choices)
         message_str = "@everyone, il est venu l'heure d'un nouveau sondage !"
         if remaining is not None:
             message_str += f" Il me reste {remaining} questions en stock"
@@ -44,21 +56,15 @@ class MyClient(discord.Client):
         next_time = incr_time(datetime.now()).strftime("%H:%M:%S - %m/%d/%Y")
         message.remove_reaction()
         print(f"Successfuly sent poll! Next poll at {next_time}")
-        
+
     async def send_homemade_poll(self, channel: discord.abc.GuildChannel, question, remaining: int = None):
-        choices = []
-        for member in channel.members:
-            if member.bot:
-                continue
-            username = member.nick if member.nick else member.name
-            choices.append(username)
-            
+        choices = list(self.get_nickname_map(channel).values())
         poll_data = Poll(question, choices, POLL_DELTA)
-        
+
         message_str = "@everyone, il est venu l'heure d'un nouveau sondage !"
         if remaining is not None:
-            message_str += f" Il me reste {remaining} questions en stock"            
-       
+            message_str += f" Il me reste {remaining} questions en stock"
+
         poll_message: discord.Message = await channel.send(message_str, embed=poll_data.get_front_embed())
         for _, reaction in poll_data.choices.items():
             await poll_message.add_reaction(reaction)
@@ -67,17 +73,14 @@ class MyClient(discord.Client):
         next_time = incr_time(datetime.now()).strftime("%H:%M:%S - %m/%d/%Y")
         self.data_base.add_active_poll(poll_data, poll_message.id, result_message.id)
         print(f"Successfuly sent poll! Next poll at {next_time}")
-        
+
     @tasks.loop(seconds=30)
     async def my_background_task(self):
         self.data_base.close_polls()
-        
+
         last_poll_ts = self.data_base.get_last_poll()
         if datetime.now() < incr_time(datetime.fromtimestamp(last_poll_ts)):
             return
-        question = self.data_base.get_random_question()
-        if question is None:
-            question = "Je n'ai plus de question à poser (c'est bien réel), qui doit régler ce problème ?\n"
         guild = discord.utils.get(self.guilds, id=GUILD_ID)
         if not guild:
             print(f"No guild found with id {GUILD_ID}")
@@ -86,7 +89,9 @@ class MyClient(discord.Client):
         if not channel:
             print(f"No channel found with name {CHANNEL_NAME}")
             return
-        
+        question = self.data_base.get_random_question()
+        if question is None:
+            question = "Je n'ai plus de question à poser (c'est bien réel), qui doit régler ce problème ?\n"
         await self.send_homemade_poll(channel, question, remaining=len(self.data_base.remaining_questions()))
         self.data_base.update_last_poll()
 
@@ -105,9 +110,8 @@ class MyClient(discord.Client):
         elif message.content.startswith("/remaining"):
             n = len(self.data_base.remaining_questions())
             await message.channel.send(f"Il reste {n} questions")
-            
+
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        """Gives a role based on a reaction emoji."""
         for poll_message_id, info in self.data_base.get_polls():
             if payload.message_id != poll_message_id:
                 continue
@@ -127,14 +131,15 @@ class MyClient(discord.Client):
             info["poll_data"].votes[payload.member.id] = choice
             await result_msg.edit(embed=info["poll_data"].get_thread_embed(self.get_nickname_map(channel)))
             return
-        
+
     def get_nickname_map(self, channel):
-        
+
         nick_map = {}
         for member in channel.members:
-            nick_map[member.id] = member.nick if member.nick else member.name
+            if member.bot:
+                continue
+            nick_map[member.id] = USER_MAP[member.id] if member.id in USER_MAP.keys() else member.name
         return nick_map
-        
 
 
 def main():
@@ -144,6 +149,5 @@ def main():
     client.run(TOKEN)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
